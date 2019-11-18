282dfdf26f3e00bc5ee4976a398b123d660d9123
Sara Fotros <sarafotros1@yahoo.com>
2019-11-18 15:06:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,5 @@
-my_sentence = { %[hello again pro]}
-my_sentence.each do {|word| word.reverse}
+def reverse_each_word(my_sentence)
+  my_sentence = %w[hello again]
+  #my_sentence.each do { |word| word.reverse }
+  my_sentence.split.collect {|word| word.reverse}.join(" ")
 end
